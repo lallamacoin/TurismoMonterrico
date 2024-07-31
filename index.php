496c30b7dee76676a4ac404f6dc4dc15e90e3e8d
lallamacoin <44884513+lallamacoin@users.noreply.github.com>
2024-07-31 00:14:17 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+        <?php include 'cabecera.php'; ?>
+        
+        <section class="page-section clearfix">
+            <div class="container">
+                <div class="intro">
+                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/intro.jpg" alt="..." />
+                    <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+                        <h2 class="section-heading mb-4">
+                            <span class="section-heading-upper">Tradición y Trabajo</span>
+                            <span class="section-heading-lower">Monterrico</span>
+                        </h2>
+                        <p class="mb-3">Monterrico tiene una larga tradición productiva agropecuaria, es por ello se la declara Capital Provincial del Tabaco. durante la primera mitad del siglo xx llega una fuerte corriente imigratoria de españoles e italianos, estos traen consigo los cultivos que hacian en su tierra de origen, hoy los nietos y bisnietos estan regresando a los mismos</p>
+                        <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="#!">Visit Us Today!</a></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section cta">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                        <div class="cta-inner bg-faded text-center rounded">
+                            <h2 class="section-heading mb-4">
+                                <span class="section-heading-upper">Monterrico</span>
+                                <span class="section-heading-lower">la experiencia</span>
+                            </h2>
+                            <p class="mb-0">Descubre Monterrico: un rincón donde cada experiencia es única e irrepetible.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<?php include 'pie.php'; ?>   
